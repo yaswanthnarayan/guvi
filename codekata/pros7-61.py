68a354775c1d5638aa73908dd668b9cd20bee9ed
yaswanthnarayan <48712422+yaswanthnarayan@users.noreply.github.com>
2019-06-14 19:44:19 +0530

    Update pros7-61.py

--- codekata/pros7-61.py
+++ codekata/pros7-61.py
@@ -5,7 +5,7 @@ for i in range(len(s)):
     r=ord(t[i])
     l=((n-96)+(r-96))
     if(l > 26)and(l%26!=0):
-        l=(((n-96)+(r-96))%26)+96)
+        l=(((n-96)+(r-96))%26)+96
         print(chr(l),end='')
     if(l%26==0):
         l=122
