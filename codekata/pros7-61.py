aeb7004c783e0ceedb78ef68174c5312c0df2f24
yaswanthnarayan <48712422+yaswanthnarayan@users.noreply.github.com>
2019-06-14 19:31:14 +0530

    Add files via upload

---
+++ codekata/pros7-61.py
@@ -0,0 +1,15 @@
+s=input()
+t=input()
+for i in range(len(s)):
+    n=ord(s[i])
+    r=ord(t[i])
+    l=((n-96)+(r-96))
+    if(l > 26)and(l%26!=0):
+        l=(((n-96)+(r-96))%26)+96
+        print(chr(l),end='')
+    if(l%26==0):
+        l=122
+        print(chr(l),end='')
+    else:
+        l=(((n-96)+(r-96))+96)
+        print(chr(l),end='')
